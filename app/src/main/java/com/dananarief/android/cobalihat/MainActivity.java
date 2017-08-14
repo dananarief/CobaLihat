61e59d4fc14c23175432033209b31d796a98eef3
dananarief <dananarief@gmail.com>
2017-08-14 19:44:35 +0700

    Change text in screen to be more friendly to users

--- app/src/main/java/com/dananarief/android/cobalihat/MainActivity.java
+++ app/src/main/java/com/dananarief/android/cobalihat/MainActivity.java
@@ -2,12 +2,17 @@ package com.dananarief.android.cobalihat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textScreen);
+        text.setText("Oi");
     }
 }
