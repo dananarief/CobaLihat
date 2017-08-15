dd6dbba6e81a89f20d5a2aff1b38c14844e2ebef
dananarief <dananarief@gmail.com>
2017-08-15 21:28:40 +0700

    add TesModel class

---
+++ app/src/main/java/com/dananarief/android/cobalihat/TesModel.java
@@ -0,0 +1,15 @@
+package com.dananarief.android.cobalihat;
+
+/**
+ * Created by dananarief on 15/08/2017.
+ */
+
+public class TesModel {
+
+    private String name;
+
+    public TesModel(String name){
+        this.name=name;
+    }
+
+}
