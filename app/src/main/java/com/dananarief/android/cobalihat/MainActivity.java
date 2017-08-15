5827dcf76b1f49ebcf9c7455307f0c0f0b2e7f24
dananarief <dananarief@gmail.com>
2017-08-15 22:17:37 +0700

    add edittext and response text

--- app/src/main/java/com/dananarief/android/cobalihat/MainActivity.java
+++ app/src/main/java/com/dananarief/android/cobalihat/MainActivity.java
@@ -2,19 +2,44 @@ package com.dananarief.android.cobalihat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
+    EditText textfield_location;
+    TextView text_response;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         text = (TextView) findViewById(R.id.title_main);
-        text.setText("App2 dong");
+        text.setText("Please, input the location :)");
 
-        text.setText("App1");
+        text_response = (TextView) findViewById(R.id.response_main);
+
+        textfield_location = (EditText) findViewById(R.id.location_input_main);
+        textfield_location.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                text_response.setText(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
